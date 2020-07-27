30080208251b90db567499242dc3e6a759b4006d
haileybongo <haileyebongo@gmail.com>
2020-07-27 14:54:07 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+
+
+
+document.getElementById("submit").addEventListener("click", function(e){
+  e.preventDefault();
+
+  if (document.getElementById("new-task-description").value != "") {
+  let task = document.createElement("LI");
+  let content = document.createTextNode(document.getElementById("new-task-description").value);
+  task.appendChild(content);
+  document.getElementById("tasks").appendChild(task);
+  }
+
+
+})
+
 });
