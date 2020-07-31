f1ea5e2f5dd20a3eb7a5cf6de0049e04677bdc06
LeonorPDX <leonoracolbert@gmail.com>
2020-07-31 09:08:54 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  document.getElementById('create-task-form').addEventListener("submit", function(event) {
+    event.preventDefault();
+    let userInput = document.getElementById("new-task-description").value;
+    document.getElementById("create-task-form").reset();
+    let base = document.getElementById("tasks");
+    let item = document.createElement("li"); 
+    item.innerHTML = `${userInput}<button data-description="${userInput}" id="delete">X</button>`;
+    base.appendChild(item);
+    }, false);
+
+  
 });
