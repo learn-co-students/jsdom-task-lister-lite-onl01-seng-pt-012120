3112ce724ac75dbee93e7816690ef80ca0610af2
848smith <848smith@gmail.com>
2020-08-14 22:58:58 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('[type=submit]').addEventListener('click', function(e) {
+    e.preventDefault();
+    let li = document.createElement('li');
+    let newTask = document.createTextNode(e.target.previousElementSibling.value);
+    li.appendChild(newTask);
+    document.querySelector('#tasks').append(li);
+  });
 });
